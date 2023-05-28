3c54527aff8a7ebc56c5bcd5007cbf913d6fc750
Disembow <e_naletov@mail.ru>
2023-05-28 18:06:51 +0200

    feat: add footer date

--- src/components/semantic/Footer.tsx
+++ src/components/semantic/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Footer.module.scss';
 
 const Footer = () => {
-  return <footer className={styles.footer}>Footer</footer>;
+  return <footer className={styles.footer}>2023</footer>;
 };
 
 export default Footer;
